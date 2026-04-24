80a5dce4f9472f5e9c8b57e8815aa0c27c0399a2
Joaquin <joaqcn@gmail.com>
2026-04-24 11:52:24 -0400

    Add logic vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isUserOrOrgPagesRepo = repositoryName.endsWith('.github.io')
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: isUserOrOrgPagesRepo ? '/' : repositoryName ? `/${repositoryName}/` : '/',
   plugins: [react()],
 })
