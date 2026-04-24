c35c0d9070e1efe60c23b22f776ffaacee9ffd68
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2026-04-24 12:02:31 -0400

    Set base path to '/portfolio/' in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
-const isUserOrOrgPagesRepo = repositoryName.endsWith('.github.io')
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: isUserOrOrgPagesRepo ? '/' : repositoryName ? `/${repositoryName}/` : '/',
+  base: '/portfolio/',
   plugins: [react()],
 })
